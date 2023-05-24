a3b386b401fa49bde7b472c7d7b45066ce3a93e1
Brian <bstanton773@gmail.com>
2023-05-24 12:19:16 -0500

    Whiteboard

---
+++ test.py
@@ -0,0 +1,29 @@
+from whiteboard import solution
+from unittest import TestCase, main
+
+
+class TestSearchInsert(TestCase):
+
+    def test_example1(self):
+        accounts = [[1, 2, 3], [3, 2, 1]]
+        self.assertEqual(solution(accounts), 6)
+
+    def test_example2(self):
+        accounts = [[1, 5], [7, 3], [3, 5]]
+        self.assertEqual(solution(accounts), 10)
+
+    def test_example3(self):
+        accounts = [[2, 8, 7], [7, 1, 3], [1, 9, 5]]
+        self.assertEqual(solution(accounts), 17)
+
+    def test_single_account(self):
+        accounts = [[10], [20], [30]]
+        self.assertEqual(solution(accounts), 30)
+
+    def test_negative_wealth(self):
+        accounts = [[-2, -3], [-1, -3], [-4, -5]]
+        self.assertEqual(solution(accounts), -4)
+
+
+if __name__ == "__main__":
+    main()
